49e8b8516522e04ec006c68630eafbed65efcb6d
Faisal <faisal.abdullah@gmail.com>
2017-02-23 11:52:03 +0000

    simple_form gem installed and implemented

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,10 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.page(params[:page]).per(10)
+  end
+
+  def new
+    @place = Place.new
   end
 
 end
